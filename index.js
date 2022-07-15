896584cafea0557ff471036861a4c5d77ca9dadc
Sharonah8 <nyamongosharon88@gmail.com>
2022-07-15 23:52:27 +0300

    Add weekly income function

---
+++ index.js
@@ -0,0 +1,17 @@
+let shedA = 510;
+let shedB = 308;
+let shedC = 486;
+let shedD = 572;
+let totalProduction = shedA + shedB + shedC + shedD;
+let selling_price = totalProduction * 45;
+let time = 7;
+
+console.log(totalProduction);
+
+var incomeOverTime = function (selling_price, time) {
+    console.log("Your weekly income is Ksh. " + (selling_price * time));
+    console.log("Your yearly income is Ksh. " + (selling_price * (time * 52));
+}
+incomeOverTime(selling_price, time);
+
+leapYear = ["January", ... "December"];
